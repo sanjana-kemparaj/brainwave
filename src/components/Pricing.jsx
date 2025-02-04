dd2ac16b4e5eee3fcd67dd11d60e4570278cb4f2
sanjanarajj97 <sanjana.rajj97@gmail.com>
2025-02-04 12:41:16 +0100

    Initial commit: Brainwave project

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,49 @@
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import Section from "./Section";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/pricing";
+
+const Pricing = () => {
+  return (
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="relative hidden justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            className="relative z-1"
+            width={255}
+            height={255}
+          />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              className="w-full"
+              width={950}
+              height={400}
+              alt="stars"
+            />
+          </div>
+        </div>
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once use forever"
+        />
+        <div className="relative">
+          <LeftLine />
+          <RightLine />
+          <PricingList></PricingList>
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
+};
+export default Pricing;
